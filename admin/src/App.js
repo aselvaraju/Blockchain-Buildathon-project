17c26b8932ac300766ac2ea55235d22cf38fe56d
Ahmet Sen <ahmetshen.us@gmail.com>
2019-04-13 16:20:26 -0400

    working admin side

--- admin/src/App.js
+++ admin/src/App.js
@@ -33,7 +33,15 @@ class App extends Component {
 
             // Set web3, accounts, and contract to the state, and then proceed with an
             // example of interacting with the contract's methods.
-            this.setState({ web3, accounts, contract: instance }, this.runExample);
+            this.setState({ web3, accounts, contract: instance, id: "", name: "", ssn: 0, address: "", dob: "", healthhist: "", insid: 0 }, this.runExample);
+            this.updateID = this.updateID.bind(this);
+            this.updateName = this.updateName.bind(this);
+            this.updateSSN = this.updateSSN.bind(this);
+            this.updateAdr = this.updateAdr.bind(this);
+            this.updateDOB = this.updateDOB.bind(this);
+            this.updateHealthHist = this.updateHealthHist.bind(this);
+            this.updateInsID = this.updateInsID.bind(this);
+
         } catch (error) {
             // Catch any errors for any of the above operations.
             alert(
@@ -60,9 +68,9 @@ class App extends Component {
 
         // await contract.methods.registerPatient("Harry John", 635057910, "123 Main St., Buffalo, NY, 14228", "02021985", ["a, ", "b, ", "c"], "356728").send({ from: accounts[0], gas: 1000000 });
 
-        const response = await contract.methods.patients(window.location.href.split("=")[1]).call();
+        const response = await contract.methods.patients(0).call();
 
-        const responseHealthHist = await contract.methods.getPatientHealthHist(window.location.href.split("=")[1]).call();
+        const responseHealthHist = await contract.methods.getPatientHealthHist(0).call();
 
         // console.log(responseHealthHist)
 
@@ -77,9 +85,46 @@ class App extends Component {
     };
     buttonClick = async () => {
         const { accounts, contract } = this.state;
-        await contract.methods.addPatientHistory(window.location.href.split("=")[1], ", Sample Hist").send({ from: accounts[0], gas: 1000000 });
+
+        await contract.methods.registerPatient(this.state.name, this.state.ssn, this.state.address, this.state.dob, [this.state.healthhist], this.state.insid).send({ from: accounts[0], gas: 1000000 });
+
 
     }
+    updateID(event) {
+        this.setState({ id: event.target.value })
+        console.log(event.target.value)
+    }
+    updateName(event) {
+        this.setState({ name: event.target.value })
+        console.log(event.target.value)
+    }
+
+    updateSSN(event) {
+        this.setState({ ssn: event.target.value })
+        console.log(event.target.value)
+    }
+
+    updateAdr(event) {
+        this.setState({ address: event.target.value })
+        console.log(event.target.value)
+    }
+
+    updateDOB(event) {
+        this.setState({ dob: event.target.value })
+        console.log(event.target.value)
+    }
+
+    updateHealthHist(event) {
+        this.setState({ healthhist: event.target.value })
+        console.log(event.target.value)
+    }
+
+    updateInsID(event) {
+        this.setState({ insid: event.target.value })
+        console.log(event.target.value)
+    }
+
+
 
     render() {
         if (!this.state.web3) {
@@ -89,7 +134,7 @@ class App extends Component {
 
 
             <div className=" outer">
-                <form className="form-horizontal">
+                <form className="form-horizontal" >
                     <div className="panel panel-default">
                         <div className="panel-body text-center">
                             <img src="https://bootdey.com/img/Content/avatar/avatar6.png" className="img-circle profile-avatar" alt="User avatar"/>
@@ -103,31 +148,33 @@ class App extends Component {
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">ID</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValue.id}</label>
+                                    <input  onChange={this.updateID} type="text" className="form-control"/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">Name</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValue.name}</label>
+<input onChange={this.updateName} type="text" className="form-control"/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">SSN</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValue.ssn}</label>
+<input onChange={this.updateSSN} type="text" className="form-control"/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">Address</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValue.address_}</label>
+
+<input onChange={this.updateAdr}  type="text" className="form-control"/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">DOB</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValue.dob}</label>
+
+<input onChange={this.updateDOB} type="text" className="form-control"/>
                                 </div>
                             </div>
                         </div>
@@ -140,27 +187,27 @@ class App extends Component {
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">Health History</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValueHist}</label>
+
+<input onChange={this.updateHealthHist} type="text" className="form-control"/>
                                 </div>
                             </div>
 
-                            <div className="form-group">
-                                <label className="col-sm-2 control-label">New Health History</label>
-                                <div className="col-sm-10">
-                                    <input type="text" className="form-control"/>
-                                   <button onClick = {this.buttonClick.bind(this)} type="button" className="btn btn-success">Submit</button>
-                                </div>
 
-                            </div>
 
                             <div className="form-group">
                                 <label className="col-sm-2 control-label">Insurance ID</label>
                                 <div className="col-sm-10">
-                                    <label type="text" className="form-control"> {this.state.storageValue.insuranceID}</label>
+
+<input onChange={this.updateInsID} type="text" className="form-control"/>
                                 </div>
                             </div>
                         </div>
+
+
                     </div>
+                    <button onClick = {this.buttonClick.bind(this)} type="button" className="btn btn-success">Submit</button>
+
+
                 </form>
             </div>
 
